ce48c20d6becd2f9a9ae5cc563b9d326178833ce
郑健霆 <374537202@qq.com>
2022-02-23 23:20:06 +0800

    每日一题

--- daily/1994. 好子集的数目/Solution.java
+++ daily/1994. 好子集的数目/Solution.java
@@ -1,56 +1,61 @@
-// copy
+/**
+ * f[i][j] 表示选择的元素值为 i(2 <= i <= 30), 并且 30 以内的 10 个质数的使用情况为 j(10 bit) 时的好子集数目
+ * f[i][j] = f[i-1][j] 当 i 的质因子有重复时, 那么 i 肯定不能选
+ * f[i][j] = f[i-1][j] + f[i-1][j ^ mask_i] * count(i), 不选或选 i, 选 i 的前提是 j 表示的质数使用情况能够满足选择 i 的需求, 即 j & mask_i == mask_i
+ * result = f[30][(1 << 10) - 1]
+ * f[1][j] = pow(2, count(1)) 1 可选可不选
+ */
 class Solution {
-    static final int[] PRIMES = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
-    static final int NUM_MAX = 30;
+    static final int[] primes = new int[] {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+    static final int MAX_VAL = 30;
     static final int MOD = 1000000007;
 
     public int numberOfGoodSubsets(int[] nums) {
-        int[] freq = new int[NUM_MAX + 1];
-        for (int num : nums) {
-            ++freq[num];
+        int[] count = new int[MAX_VAL + 1];
+        for(int num : nums) {
+            count[num]++;
         }
 
-        int[] f = new int[1 << PRIMES.length];
-        f[0] = 1;
-        for (int i = 0; i < freq[1]; ++i) {
-            f[0] = f[0] * 2 % MOD;
-        }
-        
-        for (int i = 2; i <= NUM_MAX; ++i) {
-            if (freq[i] == 0) {
-                continue;
-            }
-            
-            // 检查 i 的每个质因数是否均不超过 1 个
-            int subset = 0, x = i;
-            boolean check = true;
-            for (int j = 0; j < PRIMES.length; ++j) {
-                int prime = PRIMES[j];
-                if (x % (prime * prime) == 0) {
-                    check = false;
-                    break;
-                }
-                if (x % prime == 0) {
-                    subset |= (1 << j);
-                }
-            }
-            if (!check) {
+        int[][] dp = new int[2][1 << 10];
+        for(int i = 1; i <= MAX_VAL; i++) {
+            if(count[i] == 0) {
                 continue;
             }
 
-            // 动态规划
-            for (int mask = (1 << PRIMES.length) - 1; mask > 0; --mask) {
-                if ((mask & subset) == subset) {
-                    f[mask] = (int) ((f[mask] + ((long) f[mask ^ subset]) * freq[i]) % MOD);
+            for(int j = 0; j <= (1 << 10) - 1; j++) {
+                if(i == 1) {
+                    dp[i % 2][j] = 1;
+                    for(int k = 0; k < count[1]; k++) {
+                        dp[i % 2][j] = dp[i % 2][j] * 2 % MOD;
+                    }
+                }
+                else {
+                    boolean check = true;
+                    int temp_i = i;
+                    int i_mask = 0;
+                    for(int k = 0; k < primes.length; k++) {
+                        int prime = primes[k];
+                        if(temp_i % (prime * prime) == 0) {
+                            check = false;
+                            break;
+                        }
+                        i_mask |= 1 << k;
+                    }
+
+                    if(!check) {
+                        dp[i % 2][j] = dp[(i - 1) % 2][j];
+                        continue;
+                    }
+
+                    dp[i % 2][j] = (j & i_mask) == i_mask ? dp[(i-1) % 2][j] + dp[(i-1) % 2][j ^ i_mask] : dp[(i-1) % 2][j];
                 }
             }
         }
 
-        int ans = 0;
-        for (int mask = 1, maskMax = (1 << PRIMES.length); mask < maskMax; ++mask) {
-            ans = (ans + f[mask]) % MOD;
-        }
-        
-        return ans;
+        return dp[MAX_VAL % 2][(1 << 10) - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().numberOfGoodSubsets(new int[] {1, 2, 3, 4}));
     }
 }
